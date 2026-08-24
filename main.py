ff1da46c86a2e14941300620848bcc1fadb144fb
Rufus Gordon-Heywood <rufus.gh@icloud.com>
2026-08-24 13:30:44 +1000

    fixed loading and chunking of data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 ###### DATA LOADING
 
 
-with open('data/input.txt', 'r+') as f:
+with open('data/input.txt', 'r') as f:
     text = f.read()
 
     chars = sorted(set(text))
@@ -12,24 +12,19 @@ with open('data/input.txt', 'r+') as f:
 
     data = np.array([char_to_idx[ch] for ch in text])
 
-    f.close()
-
 def chunk_data(data, T):
-    index = 0
 
-    n = len(data) // T
+    n = (len(data)-1) // T
 
     inputs = []
     targets = []
 
     for i in range(n):
-
+        index = i*T
         inp = data[index:(index+T)]
         tar = data[index+1:(index+1+T)]
         inputs.append(inp)
         targets.append(tar)
-
-        index += T
 
     return np.array(inputs), np.array(targets)
 
