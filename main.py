38deeb1c6d77ce36588d02685d55640196142b22
Rufus Gordon-Heywood <rufus.gh@icloud.com>
2026-09-24 11:47:57 +1000

    updated the notebook code into main.py and added argument parsing

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import numpy as np
-from scipy.special import softmax
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--train', action='store_true')
+parser.add_argument('-i', '--iterations', default='20000')
+parser.add_argument('-d', '--data', default='data/input.txt')
+parser.add_argument('-o', '--output', default='my-model')
+parser.add_argument('-m', '--model', default='model_checkpoint.npz')
+parser.add_argument('-n', '--length', default='500')
+parser.add_argument('--graph', action='store_true')
+args = parser.parse_args()
+
+def softmax(y):
+    y_shifted = np.exp(y - np.max(y))
+    return y_shifted / np.sum(y_shifted)
 
 ###### DATA LOADING
 
 
-with open('data/input.txt', 'r') as f:
+with open(args.data, 'r') as f:
     text = f.read()
 
     chars = sorted(set(text))
@@ -36,22 +50,23 @@ inputs, targets = chunk_data(data, T)
 ###### PARAMETER INIT
 
 V=len(chars)
-H = 256
+H = 512
 
-layers = 0
+layers = 2
 
-W_xh = [np.random.randn(H,V) * 0.01 for _ in range(layers)]
-W_hh = [np.random.randn(H,H) * 0.01 for _ in range(layers)]
-W_hy = np.random.randn(V,H) * 0.01
+W_xh = [np.random.randn(H,V) / np.sqrt(V)] + [np.random.randn(H,H) / np.sqrt(H) for _ in range(layers-1)]
+W_hh = [np.random.randn(H,H) / np.sqrt(H) for _ in range(layers)]
+W_hy = np.random.randn(V,H) / np.sqrt(H)
 
 b_h = [np.zeros(H) for _ in range(layers)]
 b_y = np.zeros(V)
 
-mem_W_xh = np.zeros((H,V))
-mem_W_hh = np.zeros((H,H))
 mem_W_hy = np.zeros((V,H))
-mem_b_h = np.zeros(H)
 mem_b_y = np.zeros(V)
+
+mem_W_xh = [np.zeros((H,V))] + [np.zeros((H,H)) for _ in range(layers-1)]
+mem_W_hh = [np.zeros((H,H)) for _ in range(layers)]
+mem_b_h = [np.zeros(H) for _ in range(layers)]
 
 ###### FORWARD PASS
 
@@ -62,17 +77,20 @@ def one_hot_encode(char):
 
 def forward_pass(input_chunk, target_chunk, h_prev):
     h = [np.empty((T+1, H)) for _ in range(layers)]
-    
+
     L = 0
-    x = np.empty((T, V))
+    x = np.empty(T, dtype=int)
     p = np.empty((T, V))
     for i in range(layers):
         h[i][0] = h_prev[i]
     for t in range(T):
-        x[t] = one_hot_encode(input_chunk[t])
+        x[t] = input_chunk[t]
         layer_input = x[t]
         for i in range(layers):
-            h[i][t+1] = np.tanh((W_xh[i] @ layer_input) + (W_hh[i] @ h[i][t]) + b_h[i])
+            if i == 0:
+                h[i][t+1] = np.tanh(W_xh[i][:, input_chunk[t]] + (W_hh[i] @ h[i][t]) + b_h[i])
+            else:
+                h[i][t+1] = np.tanh((W_xh[i] @ layer_input) + (W_hh[i] @ h[i][t]) + b_h[i])
             layer_input = h[i][t+1]
         y = W_hy @ h[layers - 1][t+1] + b_y
         y_shifted = np.exp(y - np.max(y))
@@ -87,138 +105,176 @@ def forward_pass(input_chunk, target_chunk, h_prev):
 ############ BACKWARD PASS
 
 def backward_pass(x, h_cache, p, target_chunk):
-    dW_xh = np.zeros((H,V))
-    dW_hh = np.zeros((H,H))
+    dW_xh = [np.zeros((H,V))] + [np.zeros((H,H)) for _ in range(layers-1)]
+    dW_hh = [np.zeros((H,H)) for _ in range(layers)]
     dW_hy = np.zeros((V,H))
-    db_h = np.zeros(H)
+    db_h = [np.zeros(H) for _ in range(layers)]
     db_y = np.zeros(V)
 
-    dh_next = np.zeros(H)
-    dh = np.zeros((T, H))
+    dh_next = [np.zeros(H) for _ in range(layers)]
+    dh_t = [np.zeros(H) for _ in range(layers)]
+    dz = [np.zeros(H) for _ in range(layers)]
 
     for t in range(T-1, -1, -1):
         dy = p[t].copy()
         dy[target_chunk[t]] -= 1
-        dW_hy += np.outer(dy, h_cache[t+1])
+        dW_hy += np.outer(dy, h_cache[1][t+1])
         db_y += dy
-        dh_t = np.transpose(W_hy) @ dy + dh_next
-        dz = dh_t * (1 - h_cache[t+1] * h_cache[t+1])
-        dW_xh += np.outer(dz, x[t])
-        dW_hh += np.outer(dz, h_cache[t])
-        db_h += dz
-        dh_next = np.transpose(W_hh) @ dz
+        dh_t[1] = np.transpose(W_hy) @ dy + dh_next[1]
+        dz[1] = dh_t[1] * (1 - h_cache[1][t+1] * h_cache[1][t+1])
+        #dW_xh += np.outer(dz, x[t])
 
-    return np.clip(dW_xh, -5, 5),\
-           np.clip(dW_hh, -5, 5),\
-           np.clip(dW_hy, -5, 5),\
-           np.clip(db_h, -5, 5),\
-           np.clip(db_y, -5, 5)
+        dW_xh[1] += np.outer(dz[1], h_cache[0][t+1])
+
+        dW_hh[1] += np.outer(dz[1], h_cache[1][t])
+        db_h[1] += dz[1]
+
+        dh_next[1] = np.transpose(W_hh[1]) @ dz[1]
+
+        dh_t[0] = np.transpose(W_xh[1]) @ dz[1] + dh_next[0]
+        dz[0] = dh_t[0] * (1 - h_cache[0][t+1] * h_cache[0][t+1])
+        dW_xh[0][:, int(x[t])] += dz[0]
+        dW_hh[0] += np.outer(dz[0], h_cache[0][t])
+        db_h[0] += dz[0]
+
+        dh_next[0] = np.transpose(W_hh[0]) @ dz[0]
+
+    dW_xh_clipped = [np.clip(g, -5, 5) for g in dW_xh]
+    dW_hh_clipped = [np.clip(g, -5, 5) for g in dW_hh]
+    db_h_clipped = [np.clip(g, -5, 5) for g in db_h]
+
+    return dW_xh_clipped, dW_hh_clipped, np.clip(dW_hy, -5, 5), db_h_clipped, np.clip(db_y, -5, 5)
 
 #dW_xh, dW_hh, dW_hy, db_h, db_y = backward_pass(x, h_cache, p, targets[0])
 
-########## ADAGRAD 
+########## ADAGRAD
 
-def adagrad(dW_xh, dW_hh, dW_hy, db_h, db_y):
-    global W_xh, W_hh, W_hy, b_h, b_y,mem_W_xh, mem_W_hh, mem_W_hy, mem_b_h, mem_b_y
-    
-    mem_W_xh += dW_xh ** 2
-    mem_W_hh += dW_hh ** 2
+def adagrad(dW_xh, dW_hh, dW_hy, db_h, db_y, learning_rate):
+    global W_xh, W_hh, W_hy, b_h, b_y, mem_W_xh, mem_W_hh, mem_W_hy, mem_b_h, mem_b_y
+
+    #learning_rate = 0.01
+    epsilon = 1e-8
+
+    for i in range(layers):
+        mem_W_xh[i] += dW_xh[i] ** 2
+        mem_W_hh[i] += dW_hh[i] ** 2
+        mem_b_h[i]  += db_h[i]  ** 2
+        W_xh[i] -= learning_rate * dW_xh[i] / (np.sqrt(mem_W_xh[i]) + epsilon)
+        W_hh[i] -= learning_rate * dW_hh[i] / (np.sqrt(mem_W_hh[i]) + epsilon)
+        b_h[i]  -= learning_rate * db_h[i]  / (np.sqrt(mem_b_h[i]) + epsilon)
+
     mem_W_hy += dW_hy ** 2
-    mem_b_h  += db_h  ** 2
-    mem_b_y  += db_y  ** 2
-
-    learning_rate = 0.1
-    epsilon = 1 * (10 ** (-8))
-
-    W_xh -= learning_rate * dW_xh / (np.sqrt(mem_W_xh) + epsilon)
-    W_hh -= learning_rate * dW_hh / (np.sqrt(mem_W_hh) + epsilon)
+    mem_b_y  += db_y ** 2
     W_hy -= learning_rate * dW_hy / (np.sqrt(mem_W_hy) + epsilon)
-    b_h -= learning_rate * db_h / (np.sqrt(mem_b_h) + epsilon)
-    b_y -= learning_rate * db_y / (np.sqrt(mem_b_y) + epsilon)
+    b_y  -= learning_rate * db_y  / (np.sqrt(mem_b_y) + epsilon)
 
 #################### SAVE THE DATA
 
-def save_model(path, W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char):
-    np.savez(path,
-             W_xh=W_xh, W_hh=W_hh, W_hy=W_hy,
-             b_h=b_h, b_y=b_y,
-             chars=np.array(list(char_to_idx.keys())))
+def save_model(path, W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char, layers):
+    save_dict = {'W_hy': W_hy, 'b_y': b_y, 'chars': np.array(sorted(char_to_idx.keys())), 'layers': layers}
+    for i in range(layers):
+        save_dict[f'W_xh_{i}'] = W_xh[i]
+        save_dict[f'W_hh_{i}'] = W_hh[i]
+        save_dict[f'b_h_{i}'] = b_h[i]
+    np.savez(path, **save_dict)
 
 ######### TRAINING LOOP
 
-import matplotlib.pyplot as plt
-
-smooth_loss = -np.log(1.0/V) * T   # reasonable init value: expected loss at random init
+smooth_loss = -np.log(1.0/V) * T
 loss_history = []
 
-h_prev = np.zeros(H)
+h_prev = [np.zeros(H) for _ in range(layers)]
 i = 0
-num_iterations = 1000000 # or however long you want to train
-'''
-for iteration in range(num_iterations):
-    if i == 0:
-        h_prev = np.zeros(H)
+num_iterations = int(args.iterations)
+learning_rate = 0.01
+reset_every = 100
 
-    L, x, h_cache, p, h_last = forward_pass(inputs[i], targets[i], h_prev)
-    dW_xh, dW_hh, dW_hy, db_h, db_y = backward_pass(x, h_cache, p, targets[i])
-    adagrad(dW_xh, dW_hh, dW_hy, db_h, db_y)
-    h_prev = h_last
+if args.train:
+    for iteration in range(num_iterations):
+        if i == 0 or iteration % reset_every == 0:
+            h_prev = [np.zeros(H) for _ in range(layers)]
 
-    smooth_loss = smooth_loss * 0.999 + L * 0.001
-    if iteration % 100 == 0:
-        loss_history.append(smooth_loss)
-        print(f"iter {iteration}, loss {smooth_loss:.4f}")
+        L, x, h_cache, p, h_last = forward_pass(inputs[i], targets[i], h_prev)
+        dW_xh, dW_hh, dW_hy, db_h, db_y = backward_pass(x, h_cache, p, targets[i])
+        learning_rate *= 0.9999995
+        adagrad(dW_xh, dW_hh, dW_hy, db_h, db_y, learning_rate)
+        h_prev = h_last
 
-    if iteration % 150000 == 0 and iteration > 0:
-        save_model(f'checkpoint_H256_{iteration}.npz', W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char)
+        smooth_loss = smooth_loss * 0.999 + L * 0.001
+        if iteration % 100 == 0:
+            loss_history.append(smooth_loss)
+            print(f"iter {iteration}, loss {smooth_loss:.4f}")
+            print(f"learning rate: {learning_rate}")
 
-    i += 1
-    if i >= len(inputs):
-        i = 0
+        if iteration % 150000 == 0 and iteration > 0:
+            save_model(f'checkpoint_H256_{iteration}.npz', W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char, layers)
 
-save_model('model_checkpoint.npz', W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char)
+        i += 1
+        if i >= len(inputs):
+            i = 0
 
-plt.plot(loss_history)
-plt.xlabel('iteration (x100)')
-plt.ylabel('smoothed loss')
-plt.title('Training loss')
-plt.show()
+    save_model(f'{args.output}.npz', W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char, layers)
+    args.model = f'{args.output}.npz'
 
-print("Finished", num_iterations, " iterations.")
-'''
+    print("Finished", num_iterations, " iterations.")
+
+    if args.graph:
+        import matplotlib.pyplot as plt
+        plt.plot(loss_history)
+        plt.xlabel('iteration (x100)')
+        plt.ylabel('smoothed loss')
+        plt.title('Training loss')
+        plt.show()
+
 #### LOAD DATA
 
 def load_model(path):
     data = np.load(path)
-    W_xh = data['W_xh']
-    W_hh = data['W_hh']
+    layers = int(data['layers'])
+    W_xh = [data[f'W_xh_{i}'] for i in range(layers)]
+    W_hh = [data[f'W_hh_{i}'] for i in range(layers)]
+    b_h = [data[f'b_h_{i}'] for i in range(layers)]
     W_hy = data['W_hy']
-    b_h = data['b_h']
     b_y = data['b_y']
-
     chars = sorted(data['chars'].tolist())
     char_to_idx = {ch: i for i, ch in enumerate(chars)}
     idx_to_char = {i: ch for i, ch in enumerate(chars)}
+    return W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char, layers
 
-    return W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char
-
-W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char = load_model('checkpoint_H256_10950000.npz')
+W_xh, W_hh, W_hy, b_h, b_y, char_to_idx, idx_to_char, layers = load_model(args.model)
+chars = sorted(char_to_idx.keys())
+V = len(chars)
+h_prev = [np.zeros(W_hh[0].shape[0]) for _ in range(layers)]
 
 ####### RESULTS
 
-seed_char = 'a'
+print('Type the start of some text and the network will carry it on. Ctrl+C to quit.')
+while True:
+    try:
+        seed_text = input('> ')
+    except (EOFError, KeyboardInterrupt):
+        print()
+        break
 
-x = one_hot_encode(char_to_idx[seed_char])
-h = h_prev
-characters = 5000
-output = []
+    seed_text = ''.join(ch for ch in seed_text if ch in char_to_idx) or '\n'
+    characters = int(args.length)
+    output = []
 
-for i in range(characters):
-    h = np.tanh(W_xh @ x + W_hh @ h + b_h)
-    y = W_hy @ h + b_y
-    p = softmax(y)
-    new_char = np.random.choice(chars, p=p)
-    output.append(str(new_char))
-    x = one_hot_encode(char_to_idx[new_char])
+    h = [hp.copy() for hp in h_prev]
+    for ch in seed_text:
+        layer_input = one_hot_encode(char_to_idx[ch])
+        for l in range(layers):
+            h[l] = np.tanh(W_xh[l] @ layer_input + W_hh[l] @ h[l] + b_h[l])
+            layer_input = h[l]
+    for i in range(characters):
+        y = W_hy @ h[layers-1] + b_y
+        p = softmax(y)
+        new_char = np.random.choice(chars, p=p)
+        output.append(str(new_char))
+        layer_input = one_hot_encode(char_to_idx[new_char])
+        for l in range(layers):
+            h[l] = np.tanh(W_xh[l] @ layer_input + W_hh[l] @ h[l] + b_h[l])
+            layer_input = h[l]
 
-print(seed_char + ''.join(output))
+    print(seed_text + ''.join(output))
+    print('\n')
